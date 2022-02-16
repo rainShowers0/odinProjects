595e128a8061e538796081ed8de49f19d9c6dc4b
Batjargal <batjargal53@yahoo.com>
2022-02-17 01:27:01 +0800

    prepared js

--- Library/script.js
+++ Library/script.js
@@ -1,7 +1,21 @@
-let library = [];
-
 class Book {
     constructor(title, author, pages) {
-        
+        this.title = title;
+        this.author = author;
+        this.pages = pages;    
     }
+
 }
+
+let library = [];
+const books = document.getElementsByClassName('books');
+
+const book1 = new Book('Shrek', 'Howard', '23');
+library.push(book1);
+
+library.forEach(book => { 
+    const div = document.createElement('div');
+    div.className = 'book';
+    books.append(div);
+    const title = document.createElement('')
+});
