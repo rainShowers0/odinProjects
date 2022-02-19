26ff9e47fd5906bb279d84bb1fd47e6377be7c5e
Batjargal <batjargal53@yahoo.com>
2022-02-19 12:53:02 +0800

    Seems to be done, skipped the read button as it's not a core functionality

--- Library/script.js
+++ Library/script.js
@@ -1,21 +1,63 @@
 class Book {
     constructor(title, author, pages) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;    
+        this.title = `Title: ${title}`;
+        this.author = `Author: ${author}`;
+        this.pages = `Number of Pages: ${pages}`;    
     }
+}
 
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 let library = [];
-const books = document.getElementsByClassName('books');
+const books = document.getElementById('books');
+const addBtn = document.getElementById('addBtn');
+// The input fields 
+const formTitle = document.getElementById('title');
+const formAuthor = document.getElementById('author');
+const formPages = document.getElementById('pages');
 
-const book1 = new Book('Shrek', 'Howard', '23');
-library.push(book1);
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    removeChildNodes(books);
+    let newBook = new Book(formTitle.value, formAuthor.value, formPages.value,);
+    library.push(newBook);
+    
+    library.forEach(book => { 
+        if (book === undefined) return;
+        const div = document.createElement('div');
+        //  node attributes for the div
+        div.className = 'book';
+        div.setAttribute('id', `${library.length - 1}`);
 
-library.forEach(book => { 
-    const div = document.createElement('div');
-    div.className = 'book';
-    books.append(div);
-    const title = document.createElement('')
-});
+        const title = document.createElement('p');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+
+        
+        // The button to remove the Book from the HTML and the array,
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove Book';
+        removeBtn.addEventListener('click', (e) => {
+            library.splice(library[e.target.parentElement.getElementById], 1)
+            div.remove()
+        })
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+
+        div.append(title);
+        div.append(author);
+        div.append(pages);
+        div.append(removeBtn);
+        books.append(div);
+
+    });
+    // To clear the input fields
+    formTitle.value = ''
+    formAuthor.value = ''
+    formPages.value = ''
+})
